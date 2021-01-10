986a45eb312107006ba8a3071579563767b847bb
Ricardo Abreu <rjga94@gmail.com>
2021-01-10 21:24:57 +0000

    Added some more events to the audio injector

--- Assets/_Project/Scripts/Audio/AudioInjectorSO.cs
+++ Assets/_Project/Scripts/Audio/AudioInjectorSO.cs
@@ -17,6 +17,20 @@ namespace Audio
         [SerializeField, CanBeNull, LabelText("Death")] public AudioPoolSO PlayerDeath;
         [SerializeField, CanBeNull, LabelText("Loot Pickup")] public AudioPoolSO PlayerLootPickup;
 
+        [Title("Basic enemy SFX")]
+        [SerializeField, CanBeNull, LabelText("Footstep")] public AudioPoolSO EnemyFootstep;
+        [SerializeField, CanBeNull, LabelText("Attack")] public AudioPoolSO EnemyAttack;
+        [SerializeField, CanBeNull, LabelText("Hurt")] public AudioPoolSO EnemyHurt;
+        [SerializeField, CanBeNull, LabelText("Death")] public AudioPoolSO EnemyDeath;
+        
+        [Title("Poucet SFX")]
+        [SerializeField, CanBeNull, LabelText("Footstep")] public AudioPoolSO PoucetFootstep;
+        [SerializeField, CanBeNull, LabelText("Attack")] public AudioPoolSO PoucetAttack;
+        [SerializeField, CanBeNull, LabelText("Hurt")] public AudioPoolSO PoucetHurt;
+        [SerializeField, CanBeNull, LabelText("Death")] public AudioPoolSO PoucetDeath;
+        [SerializeField, CanBeNull, LabelText("Jump")] public AudioPoolSO PoucetJump;
+        [SerializeField, CanBeNull, LabelText("Stomp")] public AudioPoolSO PoucetStomp;
+        
         [Title("Level SFX")]
         [SerializeField, CanBeNull, LabelText("Background Music")] public AudioPoolSO LevelBGM;
 
