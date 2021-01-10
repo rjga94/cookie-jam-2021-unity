c0384a44c62225630ef44ef3effc770bdc51c51a
Ricardo Abreu <rjga94@gmail.com>
2021-01-10 20:30:58 +0000

    Added the replenish health over time and the red screen UI

--- Assets/_Project/Scripts/Character/HealthHandler.cs
+++ Assets/_Project/Scripts/Character/HealthHandler.cs
@@ -1,4 +1,6 @@
-﻿using Interfaces;
+﻿using System;
+using System.Collections;
+using Interfaces;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,12 +9,41 @@ namespace Character
     public class HealthHandler : MonoBehaviour, IDamageable
     {
         [SerializeField] private float health;
+        [SerializeField] private bool increaseHealthOverTime;
         public UnityEvent onTakeDamage, onDie;
 
+        private Coroutine _coroutine;
+        private float _maxHealth;
+        
         public float Health
         {
             get => health;
             private set => health = value;
+        }
+
+        private void Awake()
+        {
+            _maxHealth = Health;
+        }
+
+        private void Start()
+        {
+            if (increaseHealthOverTime) _coroutine = StartCoroutine(IncreaseHealthOverTime());
+        }
+
+        private void OnDestroy()
+        {
+            StopCoroutine(IncreaseHealthOverTime());
+            _coroutine = null;
+        }
+
+        private IEnumerator IncreaseHealthOverTime()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(.1f);
+                if (Health < _maxHealth) Health += .1f;   
+            }
         }
 
         private bool CanDie() => Health <= 0;
---
+++ Assets/_Project/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,27 @@
+﻿using Character;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+using Utilities.Extensions;
+
+namespace Managers
+{
+    public class HealthManager : SingletonMonoBehaviour<HealthManager>
+    {
+        [SerializeField] private HealthHandler _healthHandler;
+        [SerializeField] private Image _image;
+
+        private float _maxHealth;
+        
+        private void Awake()
+        {
+            _maxHealth = _healthHandler.Health;
+        }
+
+        private void Update()
+        {
+            var calculatedAlpha = _healthHandler.Health * 0.5f / _maxHealth;
+            _image.SetAlpha(-calculatedAlpha + 0.5f);
+        }
+    }
+}
